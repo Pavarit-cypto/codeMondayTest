ca201d73bb2bf94f47c6ffd7a53f33b25739d020
pavarit <pavarit.prasansup@gmail.com>
2020-12-01 10:35:41 +0700

    complete All CRUD

---
+++ src/index.ts
@@ -0,0 +1,130 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+import { pid } from "process";
+
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(express.json());
+
+/**
+ * Get All Data in Table
+ */
+app.get("/allteacher", async (req, res) => {
+  const users = await prisma.teacher.findMany();
+  res.json(users);
+});
+
+app.get("/allcourse", async (req, res) => {
+  const posts = await prisma.course.findMany();
+  res.json(posts);
+});
+
+/**
+ * Create Data in Table
+ */
+app.post(`/create/teacher`, async (req, res) => {
+  const { email, first_name, last_name, country } = req.body;
+  const {
+    course_name,
+    course_description,
+    course_length,
+    language,
+  } = req.body.course;
+  const today = new Date();
+  const birthDate = new Date(req.body.date_of_birth);
+  const age = today.getFullYear() - birthDate.getFullYear();
+  const result = await prisma.teacher.create({
+    data: {
+      email,
+      first_name,
+      last_name,
+      age: age.toString(),
+      date_of_birth: birthDate,
+      country,
+      course: {
+        create: {
+          course_name,
+          course_description,
+          course_length,
+          language,
+        },
+      },
+    },
+  });
+  res.json(result);
+});
+
+/**
+ * Delete Data in Table
+ */
+
+app.delete(`/teacher/:id`, async (req, res) => {
+  const { id } = req.params;
+  const post = await prisma.teacher.delete({
+    where: { id: Number(id) },
+  });
+  res.json(post);
+});
+
+app.delete(`/course/:id`, async (req, res) => {
+  const { id } = req.params;
+  const post = await prisma.course.delete({
+    where: { id: Number(id) },
+  });
+  res.json(post);
+});
+
+/**
+ * Update Data in Table
+ */
+app.put("/teacher/update/:id", async (req, res) => {
+  const { id } = req.params;
+  const { email, first_name, last_name, country } = req.body;
+  const today = new Date();
+  const birthDate = new Date(req.body.date_of_birth);
+  const age = today.getFullYear() - birthDate.getFullYear();
+  const teacher = await prisma.teacher.update({
+    where: { id: Number(id) },
+    data: {
+      email,
+      first_name,
+      last_name,
+      age: age.toString(),
+      date_of_birth: birthDate,
+      country,
+    },
+  });
+  res.json(teacher);
+});
+
+app.put("/course/update/:id", async (req, res) => {
+  const { id } = req.params;
+  const { course_name, course_description, course_length, language } = req.body;
+  const result = await prisma.course.update({
+    where: { id: Number(id) },
+    data: {
+      course_name,
+      course_description,
+      course_length,
+      language,
+    },
+  });
+  res.json(result);
+});
+
+/**
+ * Search Course Name
+ */
+
+app.get("/search/:input", async (req, res) => {
+  const { input } = req.params;
+  const result = await prisma.course.findOne({
+    where: { id: Number(input) },
+  })
+  res.json(result)
+})
+
+app.listen(3000, () =>
+  console.log("REST API server ready at: http://localhost:3000")
+);
